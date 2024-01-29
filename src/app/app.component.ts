42cfa0c897a0f605d7f5b8f1d1b6e49a08fb4639
Pau <byprokiller2@gmail.com>
2024-01-29 16:43:08 +0100

    feat: Add header page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable, fromEvent, map, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatButtonModule,
+    MatRippleModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'cafeteria-luna';
+export class AppComponent implements OnInit {
+  title = "Cafeteria Luna";
+  isDarkMode = false;
+
+  private media(query: string): Observable<boolean> {
+    const mediaQuery = window.matchMedia(query);
+    return fromEvent<MediaQueryList>(mediaQuery, 'change').pipe(
+      startWith(mediaQuery),
+      map((list: MediaQueryList) => list.matches)
+    );
+  }
+
+  ngOnInit(): void {
+    this.media("(prefers-color-scheme: dark)").subscribe((matches) => {
+      this.isDarkMode = matches;
+    });
+  }
 }
